c96bed25a0a22630dda8cc9e8e875c9bc9faae79
Diego Kaku <diegokaku@gmail.com>
2022-02-19 21:36:40 -0600

    basic function done, need to add badge, profile

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
-# C-9-Node.js_Professional-README-Generator
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+  # readmegen 
+
+  ## Languages
+    JavaScript,ES6,Node
+  
+  ## Description
+    gen readme
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Tests](#tests)
+  - [Contributors](#contributors)
+  - [Details](#details)
+
+  ## Installation
+  
+  Packages required to run this program are: run index
+  
+  ## Usage
+  Examples of how to use this program: create readme
+
+  ## License
+  MIT
+
+  ## Contributors
+  me
+
+  ## Tests
+  To test, run the following command: test
+
+  ## Contact
+  
+![Badge](https://img.shields.io/badge/Github-kakudiego-4cbbb9) 
+  
+![Profile Image](https://github.com/kakudiego.png?size=50)
+  
+View the project in GitHub at: kakudiego/readmegen
+  
+If you have any questions, contact the author directly at diegokaku@gmail.com.
---
+++ index.js
@@ -0,0 +1,98 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// TODO: Create an array of questions for user input
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "Enter your GitHub username:",
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "Title of your project:",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Description of the project:",
+    },
+    {
+      type: "checkbox",
+      name: "languages",
+      message: "Languages: (Check all that apply)",
+      choices: ["JavaScript", "HTML", "CSS", "ES6", "jQuery", "Bootstrap", "Node"],
+    },
+    {
+      type: "input",
+      name: "link",
+      message: "GitHub link to your project:",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Installation instructions:",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Project usage information:",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What kind of license would you like to have?",
+      choices: ["MIT", "APACHE 2.0", "GPL v3", "BSD 3", "None"],
+    },
+    {
+      type: "input",
+      name: "contributor",
+      message: "List out all contributors:",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "What command is used to run a test:",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Email for question/contact:",
+    },
+  ]);
+};
+
+// TODO: Create a function to write README file
+const writeToFile = (userInput) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile("README.md", userInput, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: "File created!",
+      });
+    });
+  });
+};
+
+// TODO: Create a function to initialize app
+async function init() {
+  try {
+    const answers = await questions();
+    generateMarkdown(answers);
+    writeToFile(generateMarkdown(answers));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Function call to initialize app
+init();
